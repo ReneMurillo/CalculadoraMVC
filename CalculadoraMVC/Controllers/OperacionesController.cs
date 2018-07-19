6fe7100d7440a4f79452e51e731ac73a09f7e55c
Rene Murillo <rene.murillo95@hotmail.com>
2018-07-18 20:58:48 -0600

    Único commit

---
+++ CalculadoraMVC/Controllers/OperacionesController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+    public partial class OperacionesController : Controller
+    {
+        // GET: Operaciones
+        public ActionResult Index()
+        { 
+            return View();
+        }
+
+        public ActionResult Suma()
+        {
+        if (Request.HttpMethod == "POST")
+        {
+            int numero1 = valor1();
+            int numero2 = valor2();
+            Suma(numero1, numero2);
+        }
+        return View();
+        }
+
+        public ActionResult Resta()
+        {
+        if (Request.HttpMethod == "POST")
+        {
+            int numero1 = valor1();
+            int numero2 = valor2();
+            Resta(numero1, numero2);
+        }
+        return View();
+        }
+
+        public ActionResult Multiplicacion()
+        {
+        if (Request.HttpMethod == "POST")
+        {
+            int numero1 = valor1();
+            int numero2 = valor2();
+            Multiplicacion(numero1, numero2);
+        }
+        return View();
+        }
+
+        public ActionResult Division()
+        {
+        if (Request.HttpMethod == "POST")
+        {
+            int numero1 = valor1();
+            int numero2 = valor2();
+            Division(numero1, numero2);
+        }
+        return View();
+        }
+
+        public ActionResult Potencia()
+        {
+            if (Request.HttpMethod == "POST")
+            {
+                int numero1 = valor1();
+                int numero2 = valor2();
+                Potencia(numero1, numero2);
+            }
+            return View();
+        }
+
+        public ActionResult Raiz()
+        {
+            if (Request.HttpMethod == "POST")
+            {
+                int numero1 = valor1();
+                Raiz(numero1);
+            }
+            return View();
+        }
+        public ActionResult TodoEnUno()
+        {
+        if (Request.HttpMethod == "POST")
+        {
+            int numero1 = valor1();
+            int numero2 = valor2();
+
+            var operacion = Request["operacion"];
+
+            if(operacion == "Suma")
+            {
+                Suma(numero1, numero2);
+            }else if (operacion == "Resta")
+            {
+                Resta(numero1, numero2);
+            }
+            else if (operacion == "Multi")
+            {
+                Multiplicacion(numero1, numero2);
+            }
+            else if (operacion == "Division")
+            {
+                Division(numero1, numero2);
+            }
+            else if (operacion == "Potencia")
+            {
+                Potencia(numero1, numero2);
+            }
+            else if (operacion == "Raiz")
+            {
+                Raiz(numero1);
+            }    
+        }
+        return View();
+    }
+
+    void Imprime(object cadena)
+        {
+            ViewBag.total += cadena;
+        }
+
+        int valor1()
+        {
+            int numero1 = Convert.ToInt32(Request["num1"]);
+            return numero1;
+        }
+        int valor2()
+        {
+            int numero2 = Convert.ToInt32(Request["num2"]);
+            return numero2;
+        }
+    }
---
+++ CalculadoraMVC/Models/Calculos.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+    public partial class OperacionesController
+    {
+        void Suma(int valor1, int valor2)
+        {
+            Imprime("El resultado de la suma es: ");
+            int suma = valor1 + valor2;
+            Imprime(suma);
+        }
+
+        void Resta(int valor1, int valor2)
+        {
+            Imprime("El resultado de la resta es: ");
+            int resta = valor1 - valor2;
+            Imprime(resta);
+        }
+        void Multiplicacion(int valor1, int valor2)
+        {
+            Imprime("El resultado de la multiplicación es: ");
+            int multi = valor1 * valor2;
+            Imprime(multi);
+        }
+
+        void Division(int valor1, int valor2)
+        {
+            if(valor2 != 0)
+            { 
+                Imprime("El resultado de la división es: ");
+                int division = valor1 / valor2;
+                Imprime(division);
+            }
+            else
+            {
+                Imprime("Imposible dividir entre 0");
+            }
+        }
+        void Potencia(int valorBase, int valorPotencia)
+        {
+            Imprime("El resultado de la potencia es: ");
+            int potencia = Convert.ToInt32(Math.Pow(valorBase, valorPotencia));
+            Imprime(potencia);
+        }
+
+        void Raiz(double valor)
+        {
+            Imprime("El resultado de la raíz es: ");
+            double potencia = Math.Sqrt(valor);
+            Imprime(Math.Round(potencia, 2));
+        }
+}
